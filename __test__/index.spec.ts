9da764720d9b790ce13c8506fdefb49bf085fcb4
yixiu <yixiu@gaoding.com>
2023-06-12 14:34:54 +0800

    fix: ci

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,7 +3,9 @@ import test from 'ava'
 import { urlsForFile } from '../index'
 
 test('sync function from native code', (t) => {
-  const result = urlsForFile('/Users/gd/Desktop/github/mac-open-with-napi/__test__/index.spec.ts')
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const result = urlsForFile(`${__dirname}/index.spec.ts`)
   console.warn(result)
   t.assert('true', 'true')
 })
