f60e80b51b5f192fc5cac7536e2d2e83068b1ede
yixiu <yixiu@gaoding.com>
2023-06-12 14:20:02 +0800

    feat: basic

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,9 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { urlsForFile } from '../index'
 
 test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+  const result = urlsForFile('/Users/gd/Desktop/github/mac-open-with-napi/__test__/index.spec.ts')
+  console.warn(result)
+  t.assert('true', 'true')
 })
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,35 @@
 #![deny(clippy::all)]
 
+use std::{path::Path};
+use napi::{bindgen_prelude::Array, Env};
 use napi_derive::napi;
+use launch_services::{application_urls_for_url, default_application_url_for_url, LSRolesMask};
+use core_foundation::url::{CFURL};
+
+#[napi(object)]
+struct AppInfo {
+	pub url: String,
+	pub is_default: bool,
+}
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
+pub fn urls_for_file(env: Env, url: String) -> Array {
+  let file_path = Path::new(&url);
+  let cf_url = CFURL::from_path(file_path, false).unwrap();
+  let app_urls = application_urls_for_url(&cf_url, LSRolesMask::ALL).unwrap();
+  let default_url = default_application_url_for_url(&cf_url, LSRolesMask::ALL).unwrap();
+  let default_absolute_path = default_url.absolute().to_path().unwrap();
+  let default_absolute_url = default_absolute_path.to_str().unwrap();
+  let mut arr = env.create_array(0).unwrap();
+
+  for url in &app_urls {
+    let absolute_path = url.absolute().to_path().unwrap();
+    let absolute_url = absolute_path.to_str().unwrap();
+    arr.insert(AppInfo {
+      url: absolute_url.to_string(),
+      is_default: absolute_url == default_absolute_url,
+    }).unwrap();
+  }
+
+  arr
 }
